bb5327fd20420279878f8de91e84d6609503bfe4
User <User@DESKTOP-V5297GM>
2018-04-03 17:41:14 +0600

    Printing LCS

--- Chart/src/dp/LCS_Tabulation.java
+++ Chart/src/dp/LCS_Tabulation.java
@@ -3,7 +3,7 @@ package dp;
 public class LCS_Tabulation {
 
 	public static void main(String[] args) {
-		LCS_Recursively lcs = new LCS_Recursively();
+		LCS_Tabulation lcs = new LCS_Tabulation();
 		String s1 = "AGGTAB";
 		String s2 = "GXTXAYB";
 
@@ -12,12 +12,13 @@ public class LCS_Tabulation {
 		int m = X.length;
 		int n = Y.length;
 
-		System.out.println("Length of LCS is" + " " + lcs.lcs(X, Y, m, n));
+		System.out.println("Length of LCS is" + " " + lcs.lcs(X, Y, m, n, s1, s2));
 
 	}
 
 	/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
-	int lcs(char[] X, char[] Y, int m, int n) {
+	int lcs(char[] X, char[] Y, int m, int n, String s1, String s2) {
+
 		int L[][] = new int[m + 1][n + 1];
 
 		/*
@@ -34,6 +35,50 @@ public class LCS_Tabulation {
 					L[i][j] = max(L[i - 1][j], L[i][j - 1]);
 			}
 		}
+
+		// Following code is used to print LCS
+		int index = L[m][n];
+		int temp = index;
+
+		// Create a character array to store the lcs string
+		char[] lcs_print = new char[index + 1];
+		lcs_print[index] = '\0'; // Set the terminating character
+
+		// Start from the right-most-bottom-most corner and
+		// one by one store characters in lcs[]
+		String XX = s1;
+		String YY = s2;
+
+		int i = m, j = n;
+		while (i > 0 && j > 0) {
+			// If current character in X[] and Y are same, then
+			// current character is part of LCS
+			if (XX.charAt(i - 1) == YY.charAt(j - 1)) {
+				// Put current character in result
+				lcs_print[index - 1] = XX.charAt(i - 1);
+
+				// reduce values of i, j and index
+				i--;
+				j--;
+				index--;
+			}
+
+			// If not same, then find the larger of two and
+			// go in the direction of larger value
+			else if (L[i - 1][j] > L[i][j - 1])
+				i--;
+			else
+				j--;
+		}
+
+		// Print the lcs
+		// System.out.print("LCS of " + XX + " and " + YY + " is ");
+		for (int k = 0; k <= temp; k++) {
+			System.out.print(lcs_print[k]);
+		}
+
+		System.out.println();
+
 		return L[m][n];
 	}
 
